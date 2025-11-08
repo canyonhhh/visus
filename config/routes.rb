506431652b882199900fd644fe032b307d368521
Canyon <jorislisas@gmail.com>
2025-11-08 17:50:52 +0200

    rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get "activities/index"
+  get "activities/show"
+  get "activities/new"
+  get "activities/create"
+  get "activities/edit"
+  get "activities/update"
+  get "activities/destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
