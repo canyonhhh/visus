73903c0dac0891f7c94647228c3f20bdad765b54
Canyon <jorislisas@gmail.com>
2025-11-11 21:58:52 +0200

    different structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  resources :qr_codes
-  resources :device_fingerprints
-  resources :attendance_records
-  resources :students
-  resources :activities
-  resources :employees
-  resources :businesses
-
   get "up" => "rails/health#show", as: :rails_health_check
 end
